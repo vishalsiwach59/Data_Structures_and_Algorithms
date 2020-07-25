6bae9fa09a7dc03643812732a8e21e503317f397
vishalsiwach59 <43112695+vishalsiwach59@users.noreply.github.com>
2020-07-26 00:09:21 +0530

    Add files via upload

---
+++ DP/Jump_Game_II.cpp
@@ -0,0 +1,33 @@
+//https://leetcode.com/problems/jump-game-ii/submissions/
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n=nums.size();
+        if(n==0||n==1){
+            return 0;
+        }
+        if(nums[0]==0){
+            return -1;
+        }
+        int maxreach=nums[0];
+        int step=nums[0];
+        int jump=1;
+        int i=1;
+        while(i<n){
+            if(i==n-1){
+                return jump;
+            }
+            maxreach=max(maxreach,i+nums[i]);
+            step--;
+            if(step==0){
+                jump++;
+                if(i>=maxreach){
+                    return -1;
+                }
+                step=maxreach-i;
+            }
+            i++;
+        }
+        return -1;
+    }
+};
